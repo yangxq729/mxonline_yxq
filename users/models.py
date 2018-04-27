3d8accf45612033b4f7e4b7b839bc84ce7ca50d6
yangxq729 <yangxq729@163.com>
2018-04-27 10:44:56 +0800

    mod by yxq @ 20180427 for 邮箱验证model,轮播图model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
 # Create your models here.
 class UserProfile(AbstractUser):
@@ -28,4 +29,34 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+#邮箱验证码model
+class EmailVerifyRecord(models.Model):
+    SEND_CHIOCES = (
+        ("register",u"注册"),
+        ("forget",u"找回密码")
+    )
+    code = models.CharField(max_length=20,verbose_name=u"验证码")
+    email = models.EmailField(max_length=50,verbose_name=u"邮箱")
+    send_type = models.CharField(choices=SEND_CHIOCES,max_length=10)
+    send_time = models.DateTimeField(default=datetime.now)
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+#轮播图model
+class Banner(models.Model):
+    title = models.CharField(max_length=100,verbose_name=u"标题")
+    image = models.ImageField(upload_to="banner/%Y/%m",verbose_name=u"轮播图",max_length=100)
+    url = models.URLField(max_length=100,verbose_name=u"访问地址")
+    index = models.IntegerField(default=100,verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now,verbose_name=u"添加时间")
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
+
+
+
+
+
 
