9dbe40276a11c4ec5d72c58a5fb766ce0ab684b7
yangxq729 <yangxq729@163.com>
2018-05-09 11:13:27 +0800

    mod by yxq @20180509

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class RegisterView(View):
+    '''用户注册'''
+    def get(self,request):
+        return render(request,'register.html')
